53696886e3a99a78031495cdfed722272450ed98
Kore <0so08ks@farmdeu.com>
2021-02-17 14:06:07 +0100

    fix mango

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -1,13 +1,6 @@
 import mongoose from "mongoose"
 
-const MONGO_USERNAME = "db"
-const MONGO_PASSWORD = "root"
-const MONGO_HOSTNAME = "gra"
-const MONGO_PORT = 27017
-const MONGO_DB = "gra-info"
-
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
-  
+const url = `mongodb+srv://kynda:kynda45*@gracoin.uezqr.mongodb.net/kynda?retryWrites=true&w=majority`;
 
 
 // const url = "mongodb://127.0.0.1:8089/mydb"
